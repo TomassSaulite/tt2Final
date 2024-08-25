0bb889ce98ef3dd353de7d376eb603f29597c27e
Tomass <tomassaulite@gmail.com>
2024-08-26 00:04:39 +0300

    partially done login(problem with views, doesnt allow login window open)

--- finalProject/users/forms.py
+++ finalProject/users/forms.py
@@ -16,4 +16,30 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ( 'email', 'password1','password2')
-        help_texts = { 'username': None, 'email': None,'password1': None, 'password2': None,'password':None }
+
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
+
+class CustomLoginForm(forms.Form):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email', 'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control'}))
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+
+        if email and password:
+            self.user = authenticate(self.request, email=email, password=password)
+            if self.user is None:
+                raise forms.ValidationError('Invalid email or password')
+        return self.cleaned_data
+
+    def get_user(self):
+        return self.user
+        
+        
--- finalProject/users/views.py
+++ finalProject/users/views.py
@@ -23,3 +23,22 @@ def signup(request):
     else:
         form = SignupForm()
     return render(request, 'signup.html', {'form': form})
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
+from .forms import CustomLoginForm
+
+def custom_login_view(request):
+    form = CustomLoginForm(request=request, data=request.POST or None)
+    
+    if request.method == 'POST':
+        if form.is_valid():
+            user = form.get_user()  # Fetch the authenticated user
+            if user:
+                login(request, user)  # Correct usage of login() function
+                messages.success(request, 'You have successfully logged in.')
+                return redirect('home')  # Replace 'home' with your desired success URL
+
+    return render(request, 'login.html', {'form': form})
+
